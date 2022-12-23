34927149577362831624c63998514e0c6cbab3dc
StefanYves <stefannisipeanu02@gmail.com>
2022-12-23 03:45:38 +0200

    Added all 3 buttons and finished app

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var btn2 = document.getElementById("btn2");
 var btn3 = document.getElementById("btn3");
 var audio1 = new Audio("audio1.mp3");
 var audio2 = new Audio("audio2.mp3");
+var audio3 = new Audio("audio3.mp3");
 
 btn1.addEventListener("click", checkEngineSound);
 btn2.addEventListener("click", hazardLightsSound);
+btn3.addEventListener("click", lockUnlockSound);
 
 function checkEngineSound() {
   let img1 = document.getElementById("img1");
@@ -31,8 +33,39 @@ function hazardLightsSound() {
     img2.removeAttribute("hidden");
     hidden = false;
 
+    let interval = setInterval(function () {
+      if (img2.hasAttribute("hidden")) {
+        img2.removeAttribute("hidden");
+      } else {
+        img2.setAttribute("hidden", "");
+      }
+    }, 500);
+
     audio2.onended = function () {
+      clearInterval(interval);
       img2.setAttribute("hidden", "");
     };
+  }
+}
+
+let lock = true;
+
+function lockUnlockSound() {
+  let img3 = document.getElementById("img3");
+
+  audio3.play();
+
+  if (lock) {
+    img3.setAttribute(
+      "src",
+      "https://cdn-icons-png.flaticon.com/512/61/61457.png"
+    );
+    lock = false;
+  } else {
+    img3.setAttribute(
+      "src",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Unlock_icon.svg/1200px-Unlock_icon.svg.png"
+    );
+    lock = true;
   }
 }
